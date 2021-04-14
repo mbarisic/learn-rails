82f65dc86558280c973d518972f78df62848f66d
milka <46106052+devana257@users.noreply.github.com>
2021-04-14 18:29:01 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
+    flash.now[:notice] = "Notice this flash msg!"
+    flash.now[:alert] = "This is one alert notice!"
   end
 end
