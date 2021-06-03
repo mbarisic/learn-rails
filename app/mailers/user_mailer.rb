b0a90a2e97d93bb02f5db546e081f4b60de32d78
milka <46106052+devana257@users.noreply.github.com>
2021-06-03 12:15:54 +0200

    user_mailer default email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  # default from: "donotreply@example.com"
+  default from: "mlkbarisic@gmail.com"
   def contact_email(contact)
     @contact = contact
     mail(to: Rails.application.secrets.owner_email, from_contact: @contact.email, :subject => "Website Contact")
